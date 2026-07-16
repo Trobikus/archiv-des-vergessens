7999d728f0f23141dcc874a986a9bfaa666dda7c
Xadlas <hoffmannj@mail.de>
2026-07-16 02:19:45 +0200

    Add GitHub Pages deployment workflow

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // Setzt den korrekten Pfad für das GitHub-Pages-Unterverzeichnis
+    base: 'https://github.com/Trobikus/archiv-des-vergessens',
+})
